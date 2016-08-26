a5674ca4796735f23d36ff24de6f88d210e36552
Dustin Deus <deusdustin@gmail.com>
2016-08-26 12:51:43 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # seneca-tests
 Seneca is a microservice toolkit to build distributed systems. In order to guarantee that your most important part of your application works as you expect I write some edges cases to prove senecajs in practice. Thanks to the guys who create senecajs.
+
+# Run tests
+
+```js
+mocha
+````
+
+# Description
+In Seneca you can build easily a network of Microservices. Seneca built this tool of following reasons:
+
+> So that it doesn't matter,
+
+    who provides the functionality,
+    where it lives (on the network),
+    what it depends on,
+    it's easy to define blocks of functionality (plugins!).
+    So long as some command can handle a given JSON document, you're good.
+
+**Where it lives (on the network)**
+
+To ensure this feature senecajs needs to be able to handle distribution problems. I open this repository fo focus on error handling in a microservice environment.
+Any contribution is welcome! Thank you. In case of the wrong usage of this framework it would be nice to show me the correct way. Another reason why I open this is that senecajs lacks on documentation.
+
+### Author
+Dustin Deus <deusdustin@gmail.de>
---
+++ test/errors.spec.js
@@ -0,0 +1,277 @@
+'use strict'
+
+const Seneca = require('seneca');
+const Assert = require('assert');
+
+describe('Seneca error handling', function () {
+
+    describe('Error handling in a call chain()', function () {
+
+        it('should propagate passed error to the first callee', function (done) {
+
+            function A() {
+                this.add('cmd:A', (msg, done) => {
+                    this.act('cmd:B', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to A-B call :', err);
+                        }
+                        return done(null, {chain: 'A->B->' + reply.chain})
+                    });
+                })
+            }
+
+            function B() {
+                this.add('cmd:B', (msg, done) => {
+                    this.act('cmd:C', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to B->C call :', err);
+                        }
+                        return done(null, {chain: 'C->' + reply.chain})
+                    });
+                })
+            }
+
+            function C() {
+                this.add('cmd:C', (msg, done) => {
+                    return done(new Error('test'));
+                })
+            }
+
+            let server = Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .use(A)
+                .use(B)
+                .use(C)
+                .error((err) => {
+                    console.error(err);
+                })
+                .listen({type: 'http', port: '8260', pin: 'cmd:*'});
+
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .client({port: 8260, pin: 'cmd:*'})
+                .error((err) => {
+                    console.error(err);
+                })
+                .act('cmd:A', function (err, reply) {
+                    Assert.ok(err); //Important: You should be able to evaluate the error for yourself
+                    console.log('error: ', err, 'result: ', reply);
+                    server.close();
+                    done();
+                });
+
+
+
+        });
+
+        it('should propagate timeout errors to the first callee', function (done) {
+
+            function A() {
+                this.add('cmd:A', (msg, done) => {
+                    this.act('cmd:B', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to A-B call :', err);
+                        }
+                        return done(null, {chain: 'A->B->' + reply.chain})
+                    });
+                })
+            }
+
+            function B() {
+                this.add('cmd:B', (msg, done) => {
+                    this.act('cmd:C', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to B->C call :', err);
+                        }
+                        return done(null, {chain: 'C->' + reply.chain})
+                    });
+                })
+            }
+
+            function C() {
+                //NOTE: D is not available so it leads to a timeout error
+                this.add('cmd:D', (msg, done) => {
+                    return done(new Error('test'));
+                })
+            }
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .use(A)
+                .use(B)
+                .use(C)
+                .error((err) => {
+                    console.error(err);
+                })
+                .listen({type: 'http', port: '8261', pin: 'cmd:*'});
+
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .client({port: 8261, pin: 'cmd:*'})
+                .error((err) => {
+                    console.error(err);
+                })
+                .act('cmd:A', function (err, reply) {
+                    Assert.ok(err) //Important: You should be able to evaluate the error for yourself
+                    console.log('error: ', err, 'result: ', reply);
+                    done();
+                });
+
+
+
+        });
+
+        it('should propagate passed error to the previous callee', function (done) {
+
+            function A() {
+                this.add('cmd:A', (msg, done) => {
+                    this.act('cmd:B', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to A-B call :', err);
+                        }
+                        return done(null, {chain: 'A->B->' + reply.chain})
+                    });
+                })
+            }
+
+            function B() {
+                this.add('cmd:B', (msg, done) => {
+                    this.act('cmd:C', function (err, reply) {
+
+                        Assert.ok(err); //Important: You should be able to evaluate the error for yourself
+
+                        if (err) {
+                            console.error('Error propagated to B->C call :', err);
+                        }
+                        return done(null, {chain: 'C->' + reply.chain})
+                    });
+                })
+            }
+
+            function C() {
+                this.add('cmd:C', (msg, done) => {
+                    return done(new Error('test'));
+                })
+            }
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .use(A)
+                .use(B)
+                .use(C)
+                .error((err) => {
+                    console.error(err);
+                })
+                .listen({type: 'http', port: '8262', pin: 'cmd:*'});
+
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .client({port: 8262, pin: 'cmd:*'})
+                .error((err) => {
+                    console.error(err);
+                })
+                .act('cmd:A', function (err, reply) {
+                    console.log('error: ', err, 'result: ', reply);
+                    done();
+                });
+
+
+
+        });
+
+        it('should passed error in a custom response - This is a dirty hack', function (done) {
+
+            function A() {
+                this.add('cmd:A', (msg, done) => {
+                    this.act('cmd:B', function (err, reply) {
+                        if (err) {
+                            console.error('Error propagated to A-B call :', err);
+                        }
+                        return done(null, {chain: 'A->B->' + reply.chain})
+                    });
+                })
+            }
+
+            function B() {
+                this.add('cmd:B', (msg, done) => {
+                    this.act('cmd:C', function (err, reply) {
+
+                        if (err) {
+                            console.error('Error propagated to B->C call :', err);
+                        }
+                        return done(null, {chain: 'C->' + reply.chain})
+                    });
+                })
+            }
+
+            function C() {
+                this.add('cmd:C', (msg, done) => {
+                    return done(null /*always null*/, { success: false, chain: 'error'  });
+                })
+            }
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .use(A)
+                .use(B)
+                .use(C)
+                .error((err) => {
+                    console.error(err);
+                })
+                .listen({type: 'http', port: '8263', pin: 'cmd:*'});
+
+
+            Seneca({
+                debug: {
+                    undead: true
+                },
+                log: 'silent'
+            })
+                .client({port: 8263, pin: 'cmd:*'})
+                .error((err) => {
+                    console.error(err);
+                })
+                .act('cmd:A', function (err, reply) {
+                    Assert.ok(!err); //Err is ignored everywhere
+                    Assert.ok(reply.chain, 'A->B->C->error');
+                    done();
+                });
+
+
+
+        });
+
+    });
+
+});
